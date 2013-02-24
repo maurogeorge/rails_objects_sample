b2d77c21a9e8a39722682b0035f91cc2b53e4091
Mauro George <maurogot@gmail.com>
2013-02-24 11:35:40 -0300

    added mass assigment to Post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Post do
+
+  describe "mass assignment" do
+
+    context "allowed values" do
+      [:content, :title].each do |attr|
+        it { should allow_mass_assignment_of(attr) }
+      end
+    end
+
+    context "not allowed values" do
+      [:id, :user_id, :created_at, :updated_at].each do |attr|
+        it { should_not allow_mass_assignment_of(attr) }
+      end
+    end
+  end
+end
