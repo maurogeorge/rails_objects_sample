d213fda644a137068b162e42664585251423a522
Mauro George <maurogot@gmail.com>
2013-02-24 12:21:21 -0300

    fixed indentation

--- spec/routing/pages_routing_spec.rb
+++ spec/routing/pages_routing_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe PagesController, "routing" do
 
-    it "root_path" do
-      expect(get("/")).to route_to(controller: "pages", action: "home")
-      expect(root_path).to eq("/")
-    end
+  it "root_path" do
+    expect(get("/")).to route_to(controller: "pages", action: "home")
+    expect(root_path).to eq("/")
+  end
 end
